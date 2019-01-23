3b48f1857cda3b3c4bfaad6f518b721c13178128
GerryWen <blog@gerrywen.com>
2019-01-23 15:17:34 +0800

    提交包

---
+++ src/Illuminate/http/UploadedFile.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: PC
+ * Date: 2019/1/23
+ * Time: 15:16
+ */
+
+use Symfony\Component\HttpFoundation\File\UploadedFile as SymfonyUploadedFile;
+
+class UploadedFile extends SymfonyUploadedFile
+{
+
+}
